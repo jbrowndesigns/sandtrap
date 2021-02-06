480592e68b6c2ee935f2dd9bdd64b58653808589
Justin Brown <63377528+jbrowndesigns@users.noreply.github.com>
2021-02-06 11:34:58 -0800

    added js file

---
+++ index.js
@@ -0,0 +1,38 @@
+// Pop up
+
+$(function () {
+    var overlay = $('<div id="overlay"></div>');
+    overlay.show();
+    overlay.appendTo(document.body);
+    $(".popup").show();
+    $(".close").click(function () {
+      $(".popup").hide();
+      overlay.appendTo(document.body).remove();
+      return false;
+    });
+
+    $(".x").click(function () {
+      $(".popup").hide();
+      overlay.appendTo(document.body).remove();
+      return false;
+    });
+  });
+
+// Menu
+
+  var coll = document.getElementsByClassName("collapsible");
+  var i;
+
+  for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.maxHeight) {
+        content.style.maxHeight = null;
+      } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+      }
+    });
+  }
+
+
