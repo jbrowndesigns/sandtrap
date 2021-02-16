915aa29ca7294782d8f9d81a98b71b229149d478
Justin Brown <63377528+jbrowndesigns@users.noreply.github.com>
2021-02-15 22:12:37 -0800

    disabled popup overlay background

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 // Pop up
 
-$(function () {
-    var overlay = $('<div id="overlay"></div>');
-    overlay.show();
-    overlay.appendTo(document.body);
-    $(".popup").show();
-    $(".close").click(function () {
-      $(".popup").hide();
-      overlay.appendTo(document.body).remove();
-      return false;
-    });
+// $(function () {
+//     var overlay = $('<div id="overlay"></div>');
+//     overlay.show();
+//     overlay.appendTo(document.body);
+//     $(".popup").show();
+//     $(".close").click(function () {
+//       $(".popup").hide();
+//       overlay.appendTo(document.body).remove();
+//       return false;
+//     });
 
-    $(".x").click(function () {
-      $(".popup").hide();
-      overlay.appendTo(document.body).remove();
-      return false;
-    });
-  });
+//     $(".x").click(function () {
+//       $(".popup").hide();
+//       overlay.appendTo(document.body).remove();
+//       return false;
+//     });
+//   });
 
 // Menu
 
