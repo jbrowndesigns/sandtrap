0ed63117a023ded7692fe7513a03b5134f5fd75b
jbrowndesigns <brown.jdesign@gmail.com>
2022-02-05 23:57:58 -0800

    +Added new Popup code + Minor Changes to apperance -Removed Jquery and old JS

--- index.js
+++ index.js
@@ -1,38 +1,30 @@
-// Pop up
-
-// $(function () {
-//     var overlay = $('<div id="overlay"></div>');
-//     overlay.show();
-//     overlay.appendTo(document.body);
-//     $(".popup").show();
-//     $(".close").click(function () {
-//       $(".popup").hide();
-//       overlay.appendTo(document.body).remove();
-//       return false;
-//     });
-
-//     $(".x").click(function () {
-//       $(".popup").hide();
-//       overlay.appendTo(document.body).remove();
-//       return false;
-//     });
-//   });
 
 // Menu
+var coll = document.getElementsByClassName("collapsible");
+var i;
 
-  var coll = document.getElementsByClassName("collapsible");
-  var i;
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
 
-  for (i = 0; i < coll.length; i++) {
-    coll[i].addEventListener("click", function () {
-      this.classList.toggle("active");
-      var content = this.nextElementSibling;
-      if (content.style.maxHeight) {
-        content.style.maxHeight = null;
-      } else {
-        content.style.maxHeight = content.scrollHeight + "px";
-      }
-    });
-  }
+
+
+// Popup 
+const pop = document.querySelector('.pop');
+const closeButton = document.querySelector('.popupCloseButton');
+
+closeButton.addEventListener("click", closePop);
+
+function closePop(){
+pop.style.display="none";
+}
 
 
