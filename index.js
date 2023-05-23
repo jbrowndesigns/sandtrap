d1f6e32e54651faebd42ceef1c51824d12f9cffd
jbrowndesigns <brown.jdesign@gmail.com>
2023-05-22 19:57:04 -0700

    +Menu items +JS Code

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+"use strict"
 // Menu
 var coll = document.getElementsByClassName("collapsible");
 var i;
@@ -18,6 +18,7 @@ for (i = 0; i < coll.length; i++) {
 
 
 // Popup 
+/***
 const pop = document.querySelector('.pop');
 const closeButton = document.querySelector('.popupCloseButton');
 
@@ -26,5 +27,53 @@ closeButton.addEventListener("click", closePop);
 function closePop(){
 pop.style.display="none";
 }
+*********/
+/********* HTML
+    <!-- Pop-UP -->
+    <!-- <div class="pop">
+      <div class="pops">
+        <h1>Saint Patty's Special <i class="fas fa-beer"></i></h1>
+        <h2>March 17th</h2>
+        <h2>Corn Beef & Cabbage - $17.95</h2>
+        <h2>Irish Nachos or Reuben Sandwich - $14.95</h2>
+          <div class="popupCloseButton">&times;</div>
+          <img src="img/SaintPattys.png" alt="">
+      </div>
+    </div>        -->
+
+**********/
+
+
+function capitalizeWords() {
+  const parentElement = document.querySelector('.content dl.capitalized');
+  const dtElements = parentElement.querySelectorAll('dt');
+
+  for (const dtElement of dtElements) {
+    const text = dtElement.textContent;
+    const words = text.split(' ');
+
+    for (let i = 0; i < words.length; i++) {
+      const word = words[i];
+      if (!isNaN(word)) continue; // Skip if the word is a number
+
+      const capitalizedWord = word.charAt(0).toUpperCase() + word.slice(1);
+      words[i] = capitalizedWord;
+    }
+
+    dtElement.textContent = words.join(' ');
+  }
+}
+
+function addNewPrefix() {
+  const dtElements = document.querySelectorAll('dl dt.new');
+
+  for (const dtElement of dtElements) {
+    dtElement.textContent = 'New: ' + dtElement.textContent;
+  }
+}
+
+
+capitalizeWords();
+addNewPrefix();
 
 
